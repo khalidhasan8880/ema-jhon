d7a2c28b37b64c0e84a1d6d9795de4d2361fe608
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-03-27 12:28:23 +0600

    added header and added tailwind css or daisyui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import './Header.css'
+import image from '../../images/Logo.svg'
 const Header = () => {
     return (
-        <div>
-            <h1>ema-Jhon</h1>
+        <div className="navbar w-full rounded-none px-24 bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl"><img src={image} alt="" /></a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li className=' hover:text-primary  fistchild'><a>Order</a></li>
+                    <li className=' hover:text-primary'><a>Order Review</a></li>
+                    <li className=' hover:text-primary'><a>Manage Inventory</a></li>
+                    <li className=' hover:text-primary'><a>Login</a></li>
+                </ul>
+            </div>
         </div>
     );
 };
